e14a54ca793eaae8cfee2a59f1e3089376e5d6e7
sunnymultani111 <sunnymultani111@gmail.com>
2017-11-02 11:27:59 +0530

    update from pc to git

--- iGym/accounts/urls.py
+++ iGym/accounts/urls.py
@@ -5,9 +5,11 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+     url(r'^/$', views.index, name='index'),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^login/$', auth_views.login,
         {'template_name': 'accounts/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout,
         {'next_page': 'accounts:login'}, name='logout'),
+    url(r'^password/$', views.change_password, name='change_password'),
 ]
--- iGym/accounts/views.py
+++ iGym/accounts/views.py
@@ -1,9 +1,13 @@
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.http import request
 from accounts.forms import SignUpForm
 
 # Create your views here.
+def index(request):
+    return render(request,'accounts/index.html')
+
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -17,3 +21,21 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'accounts/signup.html', {'form': form})
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(
+                request, 'Your password was successfully updated!')
+            return redirect('accounts:change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'accounts/change_password.html', {
+        'form': form
+    })
